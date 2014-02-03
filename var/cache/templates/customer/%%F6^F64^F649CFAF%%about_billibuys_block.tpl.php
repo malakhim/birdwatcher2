6507f85bd1819e5a6756ceb15beb95876d96b27b
Bryan Wu <bryan.wu@bryonics.com>
2014-02-03 10:37:45 +0800

    Fixing about on frontpage

--- var/cache/templates/customer/%%F6^F64^F649CFAF%%about_billibuys_block.tpl.php
+++ var/cache/templates/customer/%%F6^F64^F649CFAF%%about_billibuys_block.tpl.php
@@ -1,9 +1,9 @@
-<?php /* Smarty version 2.6.18, created on 2014-01-31 21:37:46
+<?php /* Smarty version 2.6.18, created on 2014-02-03 13:35:44
          compiled from C:/wamp5/www/dutchme2/skins/basic/customer/addons/billibuys/blocks/static_templates/about_billibuys_block.tpl */ ?>
 <?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
-smarty_core_load_plugins(array('plugins' => array(array('modifier', 'trim', 'C:/wamp5/www/dutchme2/skins/basic/customer/addons/billibuys/blocks/static_templates/about_billibuys_block.tpl', 13, false),array('function', 'set_id', 'C:/wamp5/www/dutchme2/skins/basic/customer/addons/billibuys/blocks/static_templates/about_billibuys_block.tpl', 13, false),)), $this); ?>
+smarty_core_load_plugins(array('plugins' => array(array('modifier', 'trim', 'C:/wamp5/www/dutchme2/skins/basic/customer/addons/billibuys/blocks/static_templates/about_billibuys_block.tpl', 15, false),array('function', 'set_id', 'C:/wamp5/www/dutchme2/skins/basic/customer/addons/billibuys/blocks/static_templates/about_billibuys_block.tpl', 15, false),)), $this); ?>
 <?php
-fn_preload_lang_vars(array('about','about_line_1','about_line_2','about_line_3','about_line_4','find_out_more'));
+fn_preload_lang_vars(array('about','about_line_1','about_line_2','about_line_3','about_line_4','were_here_to_introduce_text','find_out_more'));
 ?>
 <?php  ob_start();  ?><?php ob_start(); ?><div class="about_heading"><?php echo fn_get_lang_var('about', $this->getLanguage()); ?>
  <span class="billibuys_coloured_1">Billi</span><span class="billibuys_coloured_2">Buys</span></div>
@@ -18,7 +18,10 @@ fn_preload_lang_vars(array('about','about_line_1','about_line_2','about_line_3',
 <br/>
 	<?php echo fn_get_lang_var('about_line_4', $this->getLanguage()); ?>
 <br/>
+	<div class="about_line_5 bottom">
+		<?php echo fn_get_lang_var('were_here_to_introduce_text', $this->getLanguage()); ?>
 
+	</div><br/>
  	<div class="find_out_how_subbtn">
 		<?php echo fn_get_lang_var('find_out_more', $this->getLanguage()); ?>
 
