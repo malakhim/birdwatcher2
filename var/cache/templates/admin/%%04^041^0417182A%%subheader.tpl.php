e8da1aa218344dafe7972e752630af0cc27fe07b
Bryan Wu <bryan.wu@bryonics.com>
2014-03-07 09:21:58 +1100

    36 clarifying navigation

--- var/cache/templates/admin/%%04^041^0417182A%%subheader.tpl.php
+++ var/cache/templates/admin/%%04^041^0417182A%%subheader.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.18, created on 2014-03-06 20:02:58
+<?php /* Smarty version 2.6.18, created on 2014-03-07 09:13:25
          compiled from common_templates/subheader.tpl */ ?>
 <?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
 smarty_core_load_plugins(array('plugins' => array(array('modifier', 'default', 'common_templates/subheader.tpl', 1, false),)), $this); ?>
